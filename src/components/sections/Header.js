be0021bf4f3537de535c050000ec13a8ca005bd2
Thuan Do PC <thuando.contact@gmail.com>
2025-04-08 19:52:22 +0700

    Add props to header

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 
 class Header extends React.Component {
     render() {
-        return (
+        const { title, subtitle } = this.props;
+
+        return (    
             <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <h1>{this.props.title}</h1>
             <button
                 id="sidebarToggleTop"
                 className="btn btn-link d-md-none rounded-circle mr-3"
