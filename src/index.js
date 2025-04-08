0509d84550a88f10a817f69ea0e58339db466a66
Thuan Do PC <thuando.contact@gmail.com>
2025-04-08 19:53:11 +0700

    them route cho Quan ly de tai

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from './pages/login';
 import AdminHome from './pages/admin';
 import AdminAccountlist from './pages/admin/account-list';
+import AdminManageTopic from './pages/admin/manage-topic';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,7 +19,7 @@ root.render(
         {/* Admin */}
         <Route path="/admin/" element={<AdminHome />} />
         <Route path="/admin/accountlist" element={<AdminAccountlist />} />
-      
+        <Route path="/admin/manage-topic" element={<AdminManageTopic />} />
       </Routes>
     </Router>
   </div>
